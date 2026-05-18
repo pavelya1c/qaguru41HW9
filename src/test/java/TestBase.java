57925caa49daa7ffa6f78aec8d38a8282f3f8272
Pavel Ya <pavelya1c@gmail.com>
2026-05-18 16:19:24 +0400

    testBaseEdit

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -9,12 +9,6 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
 
-        String remoteUrl = System.getProperty("selenide.remote");
-
-        if (remoteUrl != null) {
-            Configuration.remote = remoteUrl;
-        }
-
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
