3deede3cf06316821d671a5274cab8265c8374ca
Pavel Ya <pavelya1c@gmail.com>
2026-05-18 12:12:00 +0400

    finaCom2

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
-        Configuration.headless = false;
+        Configuration.headless = true;
 
 
 
