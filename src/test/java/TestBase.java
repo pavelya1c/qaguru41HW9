da8ff4e5344a6f51f256e6c29e70f39bad7046ef
Pavel Ya <pavelya1c@gmail.com>
2026-05-18 10:57:10 +0400

    TestBaseAdd2

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,29 +1,23 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://fstravel.com";
+
+        String remoteUrl = System.getProperty("selenide.remote");
+
+        if (remoteUrl != null) {
+            Configuration.remote = remoteUrl;
+        }
+
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
         Configuration.headless = false;
-        //  Configuration.holdBrowserOpen = false;
     }
 
     @AfterEach
     void tearDown() {
         closeWebDriver();
     }
-
 }
